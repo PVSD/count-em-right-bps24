9d932710744b5e31c47183a43077ae41c03badef
bryce_000 <bs482@pvsd.org>
2018-10-21 12:02:26 -0400

    made corrections

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
+import java.util.*;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        String inp, sp[];
+        while(true)
+        {
+            System.out.println("Type a sentence and press Enter!");
+            inp = kb.nextLine();
+            inp = (inp + "X").toUpperCase();
+
+            sp=inp.split("S\\s*A");
+            if(inp.equals("EXITX"))
+            {
+                break;
+            }
+
+            if(sp.length==2)
+            {
+                System.out.println("There is one occurrence.\n");
+            }
+            else
+            {
+                System.out.println("There are " + (sp.length-1) + " occurrences.\n" );
+            }
+        }
+
+        System.out.println("Finished!");
     }
 }
